d7ec57bee7767752323b3e7a9dda3e18456f080f
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-03-23 03:13:16 +0400

    feat: add solution for partialUsingArguments task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -202,8 +202,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...rest) => {
+    const temp = [...args1, ...rest];
+    return fn(...temp);
+  };
 }
 
 /**
