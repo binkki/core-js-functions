2606933ad6249e5fa59f248a36c86ad7e23c183d
Katerina Kulevtsova <66613405+binkki@users.noreply.github.com>
2024-03-23 03:16:53 +0400

    feat: add solution for getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -226,8 +226,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let value = startFrom;
+  return () => {
+    const temp = value;
+    value += 1;
+    return temp;
+  };
 }
 
 module.exports = {
